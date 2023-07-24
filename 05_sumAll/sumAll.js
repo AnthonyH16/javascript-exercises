3c22292861ac5948f910bb969da893b0caed7d8b
AnthonyH16 <132026902+AnthonyH16@users.noreply.github.com>
2023-07-24 15:13:09 -0400

    solve exercise 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,26 @@
-const sumAll = function() {
+let total = 0;
+const sumAll = function (first, second) {
+    if (typeof (second) == "string") {
+        total = "ERROR"
+    }
+    else if (first < second && first >= 0 && second >= 0) {
+        for (i = first; i <= second; i++) {
+            total = ((second - first) + 1) * (first + second) / 2;
+        }
+    }
+    //  total += i ;
+    else if (second < first && first >= 0 && second >= 0) {
+        for (i = second; i <= first; i++) {
+            total = ((first - second) + 1) * (first + second) / 2;
+        }
+    }
+    else {
+        total = "ERROR"
+    }
+    return total;
+}
 
-};
+
 
 // Do not edit below this line
 module.exports = sumAll;
