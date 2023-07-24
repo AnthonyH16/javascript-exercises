ca66576b68b9a4d2e964288aac2b048cc5222104
AnthonyH16 <132026902+AnthonyH16@users.noreply.github.com>
2023-07-24 15:15:03 -0400

    solve exercise 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,49 @@
-const removeFromArray = function() {
+/*const removeFromArray = function (array, ...removeThis) {
+    const result = [];
+    const removeThisList = Array.from(removeThis)
+    array.forEach((num) => {
+        if (!removeThisList.includes(num)){
+        
+        }
+         else {       
+        result.push(num)
+        } ;
+    });
+    return result;
+} */
 
-};
+
+/*const removeFromArray = function (array, ...removeThis) {
+    const result = [];
+    console.log(removeThis.length);
+    for (let num of array){                
+        if (!removeThis.includes(num)){
+            result.push(num); 
+        }
+
+    }
+    return result;
+}*/
+
+const removeFromArray = function (array, ...removeThis) {
+    //let result = [];
+    return array.filter((num) => {
+
+        return !removeThis.includes(num);
+
+    });
+
+
+
+
+    
+
+
+}
+
+
+
+
 
 // Do not edit below this line
 module.exports = removeFromArray;
